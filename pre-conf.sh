ceb1c55db5a927f6324c944e87fb55445f4518a4
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-05-03 21:23:53 -0400

    Create pre-conf.sh

---
+++ pre-conf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p /opt/xowa
+cd /opt/xowa
+wget wget https://github.com/gnosygnu/xowa/releases/download/v3.5.1.1/xowa_app_linux_64_v3.5.1.1.zip
+unzip xowa_app_linux_64_v3.5.1.1.zip
+rm xowa_app_linux_64_v3.5.1.1.zip
