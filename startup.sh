b4f5c361921d2879bdb07ac703c37589c211f83e
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-05-03 21:22:08 -0400

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,11 @@
+
+set -e
+
+if [ -f /etc/configured ]; then
+        echo 'already configured'
+else
+       
+        #needed to fix problem with ubuntu ... and cron 
+        update-locale
+        date > /etc/configured
+fi
