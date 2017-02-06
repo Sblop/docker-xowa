d0b1cf105041710b63366a14b6053448726d1539
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-02-05 21:16:04 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ -f /etc/configured ]; then
         echo 'already configured'
-        java -Xmx256m -jar /opt/xowa/xowa_linux_64.jar --app_mode http_server --http_server_port 80 &
+        java -Xmx256m -jar /opt/xowa/xowa_linux_64.jar --app_mode http_server  &
 else
         #needed to fix problem with ubuntu ... and cron 
         update-locale
         date > /etc/configured
-        java -Xmx256m -jar /opt/xowa/xowa_linux_64.jar --app_mode http_server --http_server_port 80 &
+        java -Xmx256m -jar /opt/xowa/xowa_linux_64.jar --app_mode http_server &
 fi
