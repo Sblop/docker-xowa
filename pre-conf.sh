16c9fbd2904a1aad61e411fa9f8be0dbfd846802
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-11-11 20:02:48 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -2,9 +2,9 @@
 
 mkdir -p /opt/xowa
 cd /opt/xowa
-wget https://github.com/gnosygnu/xowa/releases/download/v4.1.0.1702/xowa_app_linux_64_v4.1.0.1702.zip
-unzip xowa_app_linux_64_v4.1.0.1702.zip
-rm xowa_app_linux_64_v4.1.0.1702.zip
+wget https://github.com/gnosygnu/xowa/releases/download/v4.5.15.1709/xowa_app_linux_64_v4.5.15.1709.zip
+unzip xowa_app_linux_64_v4.5.15.1709.zip
+rm xowa_app_linux_64_v4.5.15.1709.zip
 
 #download simplewikipedia
 mkdir -p /opt/xowa/wiki
