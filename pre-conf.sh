a9a6a1dca51c659b7b10e118326c5257dd639ef3
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2018-09-07 00:20:44 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -4,7 +4,7 @@ xowa_version=v4.5.21.1808
 mkdir -p /opt/xowa
 cd /opt/xowa
 wget https://github.com/gnosygnu/xowa/releases/download/${xowa_version}/xowa_app_linux_64_${xowa_version}.zip
-unzip xowa_app_linux_${xowa_version}.zip
+unzip xowa_app_linux_64_${xowa_version}.zip
 rm xowa_app_linux_64_${xowa_version}.zip
 
 #download simplewikipedia
